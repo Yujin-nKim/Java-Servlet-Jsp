9486a182ea3040724faa23f62b3790e336502b71
yujinKim <yujinkimvv@gmail.com>
2024-01-25 12:52:02 +0900

    .

--- src/main/java/com/nhnacademy/javaservletjsp/Chapter01/n14Session/LogoutServlet.java
+++ src/main/java/com/nhnacademy/javaservletjsp/Chapter01/n14Session/LogoutServlet.java
@@ -8,22 +8,22 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class LogoutServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        // session 있으면 가져오고 없으면 null
-        HttpSession session = req.getSession(false);
-
-        if(Objects.nonNull(session)) {
-            // 세션 종료 처리
-            session.invalidate();
-        }
-        //JSESSIONID cookie 삭제 처리
-        Cookie cookie = CookieUtils.getCookie(req, "JSESSIONID");
-        if(Objects.nonNull(cookie)) {
-            cookie.setValue("");
-            cookie.setMaxAge(0);
-            resp.addCookie(cookie);
-        }
-        resp.sendRedirect("/login.html");
-    }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        // session 있으면 가져오고 없으면 null
+//        HttpSession session = req.getSession(false);
+//
+//        if(Objects.nonNull(session)) {
+//            // 세션 종료 처리
+//            session.invalidate();
+//        }
+//        //JSESSIONID cookie 삭제 처리
+//        Cookie cookie = CookieUtils.getCookie(req, "JSESSIONID");
+//        if(Objects.nonNull(cookie)) {
+//            cookie.setValue("");
+//            cookie.setMaxAge(0);
+//            resp.addCookie(cookie);
+//        }
+//        resp.sendRedirect("/login.html");
+//    }
 }

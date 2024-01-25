81985014c50d39697f3edd29238aa51562e90b21
yujinKim <yujinkimvv@gmail.com>
2024-01-25 14:22:40 +0900

    day01 14.Session

--- src/main/java/com/nhnacademy/javaservletjsp/Chapter01/n14Session/LogoutServlet.java
+++ src/main/java/com/nhnacademy/javaservletjsp/Chapter01/n14Session/LogoutServlet.java
@@ -1,29 +1,28 @@
 package com.nhnacademy.javaservletjsp.Chapter01.n14Session;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import com.nhnacademy.javaservletjsp.Chapter01.n13Cookie.CookieUtils;
+
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class LogoutServlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        // session 있으면 가져오고 없으면 null
-//        HttpSession session = req.getSession(false);
-//
-//        if(Objects.nonNull(session)) {
-//            // 세션 종료 처리
-//            session.invalidate();
-//        }
-//        //JSESSIONID cookie 삭제 처리
-//        Cookie cookie = CookieUtils.getCookie(req, "JSESSIONID");
-//        if(Objects.nonNull(cookie)) {
-//            cookie.setValue("");
-//            cookie.setMaxAge(0);
-//            resp.addCookie(cookie);
-//        }
-//        resp.sendRedirect("/login.html");
-//    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // session 있으면 가져오고 없으면 null
+        HttpSession session = req.getSession(false);
+
+        if(Objects.nonNull(session)) {
+            // 세션 종료 처리
+            session.invalidate();
+        }
+        //JSESSIONID cookie 삭제 처리
+        Cookie cookie = CookieUtils.getCookie(req, "JSESSIONID");
+        if(Objects.nonNull(cookie)) {
+            cookie.setValue("");
+            cookie.setMaxAge(0);
+            resp.addCookie(cookie);
+        }
+        resp.sendRedirect("/login.html");
+    }
 }
